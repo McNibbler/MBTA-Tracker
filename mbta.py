b70407c73ac2327285fa7835929748ec7e2f39a0
McNibbler <McNibbler@users.noreply.github.com>
2018-03-17 18:37:49 +0000

    removed a stupid file. started organizing the program

--- mbta.py
+++ mbta.py
@@ -6,11 +6,53 @@
 # Developed by: Thomas Kaunzinger         #
 ###########################################
 
+####################
+
+#####################
+# IMPORT STATEMENTS #
+#####################
+
+from abc import abc     # abstract classes
+
+
+####################
+
+###########
+# CLASSES #
+###########
+
+# World class for methods involving rendering world states
+class World(abc):
+    
+    pass
+
+
+# Class for displaying and choosing all the different stations of a line
+class Line(World):
+    
+    # Constructor method
+    def __init__(self, stations):
+        
+        self.stations = stations
+
+
+####################
+
+###############
+# MAIN METHOD #
+###############
+
 # main method weow!!
 def main():
     memes = 'bobs'
     print(memes)
 
+
+####################
+
+#############
+# EXECUTION #
+#############
 
 # Executes the main method
 if __name__ == '__main__':
