30d11224683f8f57930a521a2f827c4c5763649e
McNibbler <McNibbler@users.noreply.github.com>
2018-03-17 06:24:28 +0000

    didn't really change very much i was mostly just getting accustomed to the new linux environment i was setting up. will start actually coding this soon

--- mbta.py
+++ mbta.py
@@ -1,2 +1,18 @@
-print('test')
+###########################################
+# MBTA LCD Train Tracker for Raspberry Pi #
+# Version 0.1                             #
+# March 17, 2018                          #
+#                                         #
+# Developed by: Thomas Kaunzinger         #
+###########################################
+
+# main method weow!!
+def main():
+    memes = 'bobs'
+    print(memes)
+
+
+# Executes the main method
+if __name__ == '__main__':
+    main()
 
